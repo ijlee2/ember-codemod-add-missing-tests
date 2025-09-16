d83a4d9f2831237618b701fd3f1abaad7ec54f80
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2025-09-16 10:05:12 +0200

    feature: Allowed passing --component-structure

--- tests/helpers/shared-test-setups/my-v1-app.ts
+++ tests/helpers/shared-test-setups/my-v1-app.ts
@@ -1,10 +1,12 @@
 import type { CodemodOptions, Options } from '../../../src/types/index.js';
 
 const codemodOptions: CodemodOptions = {
+  componentStructure: 'flat',
   projectRoot: 'tmp/my-v1-app',
 };
 
 const options: Options = {
+  componentStructure: 'flat',
   projectRoot: 'tmp/my-v1-app',
 };
 
