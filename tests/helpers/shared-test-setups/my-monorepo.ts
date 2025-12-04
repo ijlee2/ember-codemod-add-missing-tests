072a810eaca30f86c08813f44c488af623c2c570
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2025-12-04 17:48:26 -0600

    refactor: Replaced normalizeFilePath() with normalize()

--- tests/helpers/shared-test-setups/my-monorepo.ts
+++ tests/helpers/shared-test-setups/my-monorepo.ts
@@ -1,19 +1,19 @@
-import { normalizeFilePath } from '@codemod-utils/files';
+import { normalize } from 'node:path';
 
 import type { CodemodOptions, Options } from '../../../src/types/index.js';
 
 const codemodOptions: CodemodOptions = {
   componentStructure: 'flat',
-  projectRoot: normalizeFilePath('tmp/my-monorepo'),
-  testAppLocation: normalizeFilePath('tests/my-v2-addon'),
+  projectRoot: normalize('tmp/my-monorepo'),
+  testAppLocation: normalize('tests/my-v2-addon'),
 };
 
 const options: Options = {
   componentStructure: 'flat',
   projectHasTemplateTag: true,
-  projectRoot: normalizeFilePath('tmp/my-monorepo'),
+  projectRoot: normalize('tmp/my-monorepo'),
   testApp: {
-    location: normalizeFilePath('tmp/my-monorepo/tests/my-v2-addon'),
+    location: normalize('tmp/my-monorepo/tests/my-v2-addon'),
     name: 'test-app',
   },
 };
