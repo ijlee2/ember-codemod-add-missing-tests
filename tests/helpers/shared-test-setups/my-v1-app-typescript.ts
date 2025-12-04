476dae52daf080229f85c6a71e96e9f83612e3e2
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2025-12-04 17:36:00 -0600

    Patched #17 (#18)
    * bugfix: Used replaceAll()
    
    * bugfix: Called normalizeFilePath()
    
    * chore: Added changeset

--- tests/helpers/shared-test-setups/my-v1-app-typescript.ts
+++ tests/helpers/shared-test-setups/my-v1-app-typescript.ts
@@ -1,15 +1,17 @@
+import { normalizeFilePath } from '@codemod-utils/files';
+
 import type { CodemodOptions, Options } from '../../../src/types/index.js';
 
 const codemodOptions: CodemodOptions = {
   componentStructure: 'flat',
-  projectRoot: 'tmp/my-v1-app-typescript',
+  projectRoot: normalizeFilePath('tmp/my-v1-app-typescript'),
   testAppLocation: undefined,
 };
 
 const options: Options = {
   componentStructure: 'flat',
   projectHasTemplateTag: true,
-  projectRoot: 'tmp/my-v1-app-typescript',
+  projectRoot: normalizeFilePath('tmp/my-v1-app-typescript'),
   testApp: undefined,
 };
 
