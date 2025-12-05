aa2fee1cdd87c34d7744caf28495d3a156d96968
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2025-12-05 08:34:14 -0600

    Patched #17 (#20)
    * refactor: Removed unnecessary normalize()'s
    
    * chore: Updated dependencies
    
    * chore: Added changeset

--- tests/helpers/shared-test-setups/my-monorepo.ts
+++ tests/helpers/shared-test-setups/my-monorepo.ts
@@ -4,14 +4,14 @@ import type { CodemodOptions, Options } from '../../../src/types/index.js';
 
 const codemodOptions: CodemodOptions = {
   componentStructure: 'flat',
-  projectRoot: normalize('tmp/my-monorepo'),
-  testAppLocation: normalize('tests/my-v2-addon'),
+  projectRoot: 'tmp/my-monorepo',
+  testAppLocation: 'tests/my-v2-addon',
 };
 
 const options: Options = {
   componentStructure: 'flat',
   projectHasTemplateTag: true,
-  projectRoot: normalize('tmp/my-monorepo'),
+  projectRoot: 'tmp/my-monorepo',
   testApp: {
     location: normalize('tmp/my-monorepo/tests/my-v2-addon'),
     name: 'test-app',
