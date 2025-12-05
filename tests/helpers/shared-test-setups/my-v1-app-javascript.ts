6cd1603a177e7219bdf2c8606594dbf945bf7d6b
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2025-12-05 08:12:01 -0600

    refactor: Removed unnecessary normalize()'s

--- tests/helpers/shared-test-setups/my-v1-app-javascript.ts
+++ tests/helpers/shared-test-setups/my-v1-app-javascript.ts
@@ -1,17 +1,15 @@
-import { normalize } from 'node:path';
-
 import type { CodemodOptions, Options } from '../../../src/types/index.js';
 
 const codemodOptions: CodemodOptions = {
   componentStructure: 'flat',
-  projectRoot: normalize('tmp/my-v1-app-javascript'),
+  projectRoot: 'tmp/my-v1-app-javascript',
   testAppLocation: undefined,
 };
 
 const options: Options = {
   componentStructure: 'flat',
   projectHasTemplateTag: true,
-  projectRoot: normalize('tmp/my-v1-app-javascript'),
+  projectRoot: 'tmp/my-v1-app-javascript',
   testApp: undefined,
 };
 
--- tests/helpers/shared-test-setups/my-v1-app-typescript.ts
+++ tests/helpers/shared-test-setups/my-v1-app-typescript.ts
@@ -1,17 +1,15 @@
-import { normalize } from 'node:path';
-
 import type { CodemodOptions, Options } from '../../../src/types/index.js';
 
 const codemodOptions: CodemodOptions = {
   componentStructure: 'flat',
-  projectRoot: normalize('tmp/my-v1-app-typescript'),
+  projectRoot: 'tmp/my-v1-app-typescript',
   testAppLocation: undefined,
 };
 
 const options: Options = {
   componentStructure: 'flat',
   projectHasTemplateTag: true,
-  projectRoot: normalize('tmp/my-v1-app-typescript'),
+  projectRoot: 'tmp/my-v1-app-typescript',
   testApp: undefined,
 };
 
